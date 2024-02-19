7350682c29796fb928f3fd92d1459f53e3077fc0
Seth Ryan DeSantis <145081141+hteslol@users.noreply.github.com>
2024-02-18 19:42:37 -0800

    Update pythonabuses.py

--- pythonabuses.py
+++ pythonabuses.py
@@ -1,4 +1,6 @@
 import sys  
  
 ''' Not sure why your being used for web API level'''
-print(sys.__dict__)
+print(sys.__dict__) 
+
+''' Full acess to system at this level in python3, which is not meaning full on back-end development.'''
